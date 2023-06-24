ecfeabb6a91ac9c9bacdf1c3e59ecb3e5af9f931
Jaewan <j1studios324@gmail.com>
2023-06-24 23:21:32 +0900

    feat: implemented queryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <Map></Map>;
+  return (
+    <QueryClientProvider client = {queryClient}>
+      <Map></Map>;
+    </QueryClientProvider>
+  );
 }
 
 export default App;
