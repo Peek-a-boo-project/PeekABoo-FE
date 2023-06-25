ef06d7ac9fee9848413da1ab5955bef9dbaacc58
Jaewan <j1studios324@gmail.com>
2023-06-25 23:38:01 +0900

    chore: setting map size & zoom

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,18 +3,16 @@ import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 import axios from 'axios';
 import { useQuery } from 'react-query';
 import LoadingScreen from './LoadingScreen';
+import { useState } from 'react';
 
 const containerStyle = {
   width: '100vw',
   height: '100vh'
 };
 
-const center = {
-  lat: -3.745,
-  lng: -38.523
-};
-
 function Map() {
+  const [center,setCenter] = useState({lat: 37.5665, lng: 126.9779});
+
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: `${process.env.REACT_APP_MAP_API}`,
@@ -24,9 +22,7 @@ function Map() {
 
   const onLoad = React.useCallback(function callback(map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-
+    map.setZoom(9)
     setMap(map)
   }, [])
 
